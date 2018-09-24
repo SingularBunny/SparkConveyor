8bd4c05a35b0947286813819288da2be99fc44ed
eugene <eugenelopatkin@gmail.com>
2018-09-24 12:41:35 +0300

    HDFS source fix

--- core/src/main/scala/com/github/singularbunny/source/HdfsSource.scala
+++ core/src/main/scala/com/github/singularbunny/source/HdfsSource.scala
@@ -1,11 +1,12 @@
 package com.github.singularbunny.source
 
-import com.github.singularbunny.param.HasOptions
+import com.github.singularbunny.param.{HasFormat, HasOptions}
 import org.apache.spark.internal.Logging
 import org.apache.spark.ml.util.Identifiable
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class HdfsSource(override val uid: String)(implicit spark: SparkSession) extends Source
+  with HasFormat
   with HasOptions
   with Logging {
 
@@ -13,10 +14,12 @@ class HdfsSource(override val uid: String)(implicit spark: SparkSession) extends
 
   override def getSourceDataFrame: DataFrame = {
     log.info(s"Read from HDFS with " +
-      "\"csv\" format " +
+      s"${$(options)} format " +
       s"${$(options)} options")
 
-    spark.read.format("csv")
+    spark
+      .read
+      .format($(format))
       .options($(options))
       .load()
   }
